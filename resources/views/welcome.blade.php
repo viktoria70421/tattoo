a3ae5af2d168629a3e2a769d1247c6b3530964ef
viktoria70421 <43682670+viktoria70421@users.noreply.github.com>
2018-10-12 19:08:08 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,10 +11,10 @@
     <title>Modern Business - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="{{ asset('/css/modern-business.css ')}}" rel="stylesheet">
 
   </head>
 
@@ -23,31 +23,31 @@
     <!-- Navigation -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="{{asset('/index.html')}}">Start Bootstrap</a>		
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
+              <a class="nav-link" href="{{asset('/about.html')}}">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="services.html">Services</a>
+              <a class="nav-link" href="{{asset('/services.html')}}">Services</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
+              <a class="nav-link" href="{{asset('/contact.html ')}}">Contact</a>			  
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Portfolio
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-                <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+                <a class="dropdown-item" href="{{ asset('/portfolio-1-col.html')}}">1 Column Portfolio</a>				
+                <a class="dropdown-item" href="{{ asset('/portfolio-2-col.html')}}">2 Column Portfolio</a>
+                <a class="dropdown-item" href="{{ asset('/portfolio-3-col.html')}}">3 Column Portfolio</a>
+                <a class="dropdown-item" href="{{ asset('/portfolio-5-col.html')}}">4 Column Portfolio</a>
+                <a class="dropdown-item" href="{{ asset('/portfolio-item.html')}}">Single Portfolio Item</a>
               </div>
             </li>
             <li class="nav-item dropdown">
@@ -55,9 +55,9 @@
                 Blog
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-                <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-                <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+                <a class="dropdown-item" href="{{asset('/blog-home-1.html')}}">Blog Home 1</a>
+                <a class="dropdown-item" href="{{asset('/blog-home-2.html')}}">Blog Home 2</a>
+                <a class="dropdown-item" href="{{asset('/blog-post.html')}}">Blog Post</a>
               </div>
             </li>
             <li class="nav-item dropdown">
@@ -65,11 +65,11 @@
                 Other Pages
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                <a class="dropdown-item" href="full-width.html">Full Width Page</a>
-                <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
-                <a class="dropdown-item" href="faq.html">FAQ</a>
-                <a class="dropdown-item" href="404.html">404</a>
-                <a class="dropdown-item" href="pricing.html">Pricing Table</a>
+                <a class="dropdown-item" href="{{asset('/full-width.html')}}">Full Width Page</a>
+                <a class="dropdown-item" href="{{asset('/sidebar.html')}}">Sidebar Page</a>
+                <a class="dropdown-item" href="{{asset('/faq.html')}}">FAQ</a>
+                <a class="dropdown-item" href="{{asset('/404.html')}}">404</a>
+                <a class="dropdown-item" href="{{asset('/pricing.html')}}">Pricing Table</a>
               </div>
             </li>
           </ul>
@@ -167,7 +167,7 @@
       <div class="row">
         <div class="col-lg-4 col-sm-6 portfolio-item">
           <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>			
             <div class="card-body">
               <h4 class="card-title">
                 <a href="#">Project One</a>
